169e5a18a1f699f84bfbbbbe2ec892697a0bad22
muhammad-z <mhd.z95@hotmail.com>
2023-02-26 13:57:44 +0300

    added techs data

---
+++ src/components/common/techs.ts
@@ -0,0 +1,21 @@
+export default [
+  {
+    name: "ReactJS",
+    imageUrl: "/icons/React-icon.svg",
+    brief: "Build user interfaces",
+    description: "I\'m an expert in ReactJS, it\'s #1 in my tech stack",
+    skill: 80
+  }, {
+    name: "Redux",
+    imageUrl: "/icons/redux-logo.svg",
+    brief: "do something with it",
+    description: "lol idk what to type here",
+    skill: 80
+  }, {
+    name: "TypeScript",
+    imageUrl: "/icons/Typescript_logo_2020.svg",
+    brief: "Make programming typed again!",
+    description: "yes I do use it",
+    skill: 50,
+  }
+]

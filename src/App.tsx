1bcce524e74441a13a4ae916a8ec0b168486f094
muhammad-z <mhd.z95@hotmail.com>
2023-02-27 22:58:40 +0300

    added global style for root

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Footer from "./footer/Footer";
+import GlobalStyle from "./styles/global";
 
 export default () => (
   <>
-  <Header />
-  <Main />
-  <Footer />
+    <GlobalStyle />
+    <Header />
+    <Main />
+    <Footer />
   </>
 );
---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  #root {
+    width: 65%;
+    margin: 0 auto;
+    min-height: 100vh;
+    border-radius: 12px;
+  }`;

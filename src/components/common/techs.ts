3605cc7939a0dd896738c68908d240332bcab898
muhammad-z <mhd.z95@hotmail.com>
2023-02-28 20:59:27 +0300

    added new techs

--- src/components/common/techs.ts
+++ src/components/common/techs.ts
@@ -10,12 +10,36 @@ export default [
     imageUrl: "/icons/redux-logo.svg",
     brief: "do something with it",
     description: "lol idk what to type here",
-    skill: 80
+    skill: 80,
+  }, {
+    name: "Javascript",
+    imageUrl: "/icons/JavaScript_logo.svg",
+    brief: "do something with it",
+    description: "lol idk what to type here",
+    skill: 90,
   }, {
     name: "TypeScript",
     imageUrl: "/icons/Typescript_logo_2020.svg",
     brief: "Make programming typed again!",
     description: "yes I do use it",
     skill: 50,
-  }
+  }, {
+    name: "Styled-Components",
+    imageUrl: "/icons/styled-components.png",
+    brief: "do something with it",
+    description: "lol idk what to type here",
+    skill: 70,
+  }, {
+    name: "Material-Ui",
+    imageUrl: "/icons/mui.svg",
+    brief: "do something with it",
+    description: "lol idk what to type here",
+    skill: 70,
+  }, {
+    name: "Git",
+    imageUrl: "/icons/git-Icon-Black.svg",
+    brief: "do something with it",
+    description: "lol idk what to type here",
+    skill: 80,
+  },
 ]

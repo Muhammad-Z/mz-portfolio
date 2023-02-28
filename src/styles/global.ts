d87d4b3508865757d7ffd82c99bd8ae38795ec74
muhammad-z <mhd.z95@hotmail.com>
2023-02-28 21:00:03 +0300

    global styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,5 +5,19 @@ export default createGlobalStyle`
     width: 65%;
     margin: 0 auto;
     min-height: 100vh;
-    border-radius: 12px;
-  }`;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+  }
+  
+  section {
+    background-color: white;
+    border: 3px solid gray;
+    margin: 1rem;
+    padding: 1rem;
+  }
+
+  h2 {
+    text-shadow: 2px 2px 10px #5499FF;
+    padding-bottom: 0.67em !important;
+  }
+  `;

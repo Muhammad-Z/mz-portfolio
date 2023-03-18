2e5462f70e50ecaced5d260de98ff45cd095b770
muhammad-z <mhd.z95@hotmail.com>
2023-03-18 22:45:20 +0300

    assists

--- src/components/overview/Technologies.tsx
+++ src/components/overview/Technologies.tsx
@@ -2,48 +2,59 @@ import * as React from "react";
 import styled from "styled-components";
 import techs from "../common/techs";
 
+interface ContainerProps {
+  readonly left?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
+width: ${(props) => props.left ? "30%" : "70%"};
+${props =>
+   props.left && `
+ display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+
+& > img {
+  cursor: pointer;
+ width: 50%;
+ height: fit-content;
+ flex-shrink: 1;
+ /* flex-grow: 1; */
+}
+
+img:hover {
+ filter: drop-shadow(0 0 0.75rem rgba(84, 153, 255, 0.65));
+}`
+ }
+
+`;
+
+const TechsPanel = styled.div`
+height: 400px;
+display: flex;
+flex-direction: row;
+gap: 0.5rem;
+justify-content: stretch;
+`;
+
 export default function Technologies() {
-  interface ContainerProps {
-    readonly left?: boolean;
+  const [selectedTech, setSelectedTech] = React.useState(0);
+
+  function handleTechClick(e: any) {
+    console.log(e.target)
+    if(e.target.tagName === "IMG") setSelectedTech(e.target.dataset.name);
   }
-  const Container = styled.div<ContainerProps>`
-   width: ${(props) => props.left ? "30%" : "70%"};
-   ${props =>
-      props.left && `
-    display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-
-  & > img {
-    width: inherit;
-    height: fit-content;
-    flex-shrink: 1;
-    flex-grow: 1;
-  }`
-    }
-
-  `
-  const TechsPanel = styled.div`
-    height: 400px;
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-    justify-content: stretch;
-  `
-
-
-
 
   return (<TechsPanel>
-    <Container left>
+    <Container left onClick={handleTechClick}>
       {
-        techs.map(tech =>
-          <img src={tech.imageUrl}></img>
+        techs.map((tech, index) =>
+          <img key={tech.name + index} src={tech.imageUrl} data-name={index}></img>
         )
       }
     </Container>
     <Container>
-      Hello
+      <p>{techs[selectedTech].description}</p>
     </Container>
 
   </TechsPanel>)
---
+++ src/components/projects/projects.ts
@@ -0,0 +1,19 @@
+export default [
+  {
+    name: "My Portfolio",
+    desktopThumbnailUrl: "/icons/React-icon.svg",
+    mobileThumbnailUrl: "",
+    brief: "developed from scratch",
+    description: "Mobile-first responsive design. static site with SEO in mind",
+    technologyUsed: [1, 2, 3, 4],
+  }, {
+    name: "Weather Forecast",
+    desktopThumbnailUrl: "/projects-shots/weather-forecast.png",
+    mobileThumbnailUrl: "",
+    brief: "lorem ipsum",
+    description: "Mobile-first responsive design. static site with SEO in mind",
+    technologyUsed: [1, 2],
+    liveSiteLink: "https://google.com",
+    sourceLink: "",
+  },
+]
